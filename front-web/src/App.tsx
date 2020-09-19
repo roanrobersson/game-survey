0681e83823932ce089acfdfec3bf5864feb7c177
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-09-18 22:28:29 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <h1>Hello semana devSuperior</h1>  
-  );
+    <div>
+      <Header />
+    </div>
+  )
 }
 
 export default App;
