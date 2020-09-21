7c8fe040c509c573283c3011fdca5581066b0fd8
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-09-20 22:58:15 -0300

    Finalizando app React Native

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
     return (
       <View style={styles.container}>
         <Routes />
-        <StatusBar style="auto" />
+        <StatusBar style="light" />
       </View>
     );
   }
